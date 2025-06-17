7ff28a56a032c7502680a52d60c532095ec74b73
bruno vettori <bruno_vettori@proton.me>
2025-06-16 21:04:29 -0300

    refactored auth

--- app/Http/Profile/Requests/ProfileUpdateRequest.php
+++ app/Http/Profile/Requests/ProfileUpdateRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Profile\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Auth\Controllers\AuthenticatedSessionController;
+use App\Http\Auth\Controllers\ConfirmablePasswordController;
+use App\Http\Auth\Controllers\EmailVerificationNotificationController;
+use App\Http\Auth\Controllers\EmailVerificationPromptController;
+use App\Http\Auth\Controllers\NewPasswordController;
+use App\Http\Auth\Controllers\PasswordController;
+use App\Http\Auth\Controllers\PasswordResetLinkController;
+use App\Http\Auth\Controllers\RegisteredUserController;
+use App\Http\Auth\Controllers\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
